d4dd490ac639b705578bd6250ab3ae931e49254e
vandruil <146136334+vandruil@users.noreply.github.com>
2025-07-27 22:18:55 +0200

    formated with autopep8

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from stats import get_word_count
 from stats import get_num_chars
 from stats import get_sort_dicts
+
+
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
         book_contents = f.read()
     return book_contents
 
 
-
-
-
-
 def main():
+    input = "books/frankenstein.txt"
+    book_text = get_book_text(input)
+    char_count = get_num_chars(book_text)
+    dict_list = get_sort_dicts(char_count)
+    word_count = get_word_count(book_text)
+    print(
+        f"============ BOOKBOT ============ \n Analyzing book found at {input}... ----------- Word Count ---------- \n Found {word_count} total words \n --------- Character Count --------- \n {dict_list}")
 
-    book_text = get_book_text("books/frankenstein.txt")
-    char_count = get_num_chars(book_text)   
-    """word_count = get_word_count(book_text)
-    print(f"{word_count} words found in the document")
-   """
-    print(char_count)
+
 main()
--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ def get_word_count(book_text):
     list_string = book_text.split()
     return len(list_string)
 
+
 def get_num_chars(book_text):
     list_string = book_text.lower()
     dic_chars = {}
@@ -12,13 +13,14 @@ def get_num_chars(book_text):
             dic_chars[char] += 1
     return dic_chars
 
+
 def get_sort_dicts(dict):
     dic_list = []
     for key in dict:
         if key.isalpha():
             new_dict = {"char": key,
-                    "num": dict[key]
-                      }
+                        "num": dict[key]
+                        }
             dic_list.append(new_dict)
-    dic_list.sort(key=lambda x: x["num"],reverse=True)
+    dic_list.sort(key=lambda x: x["num"], reverse=True)
     return dic_list
