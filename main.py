31f8e92acadad9c8cd63a6ac957a82781f6c910a
vandruil <146136334+vandruil@users.noreply.github.com>
2025-07-27 23:28:25 +0200

    removed hard coded path and added system args for unix systems

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_word_count
 from stats import get_num_chars
 from stats import get_sort_dicts
+import sys
 
 
 def get_book_text(path_to_book):
@@ -17,15 +18,19 @@ def get_print_format(dict_list):
 
 
 def main():
-    input = "books/frankenstein.txt"
-    book_text = get_book_text(input)
-    char_count = get_num_chars(book_text)
-    dict_list = get_sort_dicts(char_count)
-    word_count = get_word_count(book_text)
-    print(
-        f"============ BOOKBOT ============ \n Analyzing book found at {input}... \n ----------- Word Count ---------- \n Found {word_count} total words \n --------- Character Count ---------")
-    get_print_format(dict_list)
-    print("============= END ===============")
+    arg = sys.argv
+    if len(arg) == 2:
+        book_text = get_book_text(arg[1])
+        char_count = get_num_chars(book_text)
+        dict_list = get_sort_dicts(char_count)
+        word_count = get_word_count(book_text)
+        print(
+            f"============ BOOKBOT ============ \n Analyzing book found at {arg[1]}... \n ----------- Word Count ---------- \n Found {word_count} total words \n --------- Character Count ---------")
+        get_print_format(dict_list)
+        print("============= END ===============")
+    else:
+        print("Usage: poython3 main.py <path_to_book>")
+        sys.exit(1)
 
 
 main()
